bdf35013bec807d539517e93034a8bb98bb8a373
Ayush <spanhornet@pm.me>
2025-06-11 15:37:49 -0500

    feat(app): create theme provider with next-themes

--- apps/next-app/app/layout.tsx
+++ apps/next-app/app/layout.tsx
@@ -1,6 +1,13 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+// Next.js Metadata API
+import type { Metadata } from "next";
+
+// Google Fonts
+import { Geist, Geist_Mono } from "next/font/google";
+
+// `ThemeProvider` Component
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +30,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
