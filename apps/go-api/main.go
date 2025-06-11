c8281d0cbddcc1cb3fc3e012a5c5bef06e973778
Ayush <spanhornet@pm.me>
2025-06-10 21:48:04 -0500

    feat(go-api): initialize api with fiber framework

---
+++ apps/go-api/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{"message": "Hello, World!"})
+	})
+
+	// Start server
+	err := app.Listen(":8080")
+	if err != nil {
+		panic(err)
+	}
+}
