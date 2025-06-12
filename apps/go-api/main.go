002fa5dbb0482308c0b625881326c757d18e0674
Ayush <spanhornet@pm.me>
2025-06-12 17:08:27 -0500

    feat(go-api): implement Redis

--- apps/go-api/main.go
+++ apps/go-api/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/redis/go-redis/v9"
 
 	"github.com/joho/godotenv"
 
@@ -17,11 +19,36 @@ import (
 
 const apiVersion = "/api/v1"
 
+var rdb *redis.Client
+
+func initRedis() {
+	redisURL := os.Getenv("REDIS_URL")
+
+	opt, err := redis.ParseURL(redisURL)
+	if err != nil {
+		log.Fatalf("Failed to parse Redis URL: %v", err)
+	}
+
+	// Initialize Redis client
+	rdb = redis.NewClient(opt)
+
+	// Verify connection
+	ctx := context.Background()
+	if err := rdb.Ping(ctx).Err(); err != nil {
+		log.Fatalf("Unable to connect to Redis: %v", err)
+	}
+
+	log.Println("Connected to Redis Cloud successfully.")
+}
+
 func main() {
 	// Load environment variables
 	if err := godotenv.Load(); err != nil {
 		log.Println("no .env file found, relying on environment")
 	}
+
+	// Initialize Redis
+	initRedis()
 
 	dsn := os.Getenv("SUPABASE_DATABASE_CONNECTION_STRING")
 	if dsn == "" {
@@ -34,13 +61,18 @@ func main() {
 		log.Fatalf("error connecting to database: %v", err)
 	}
 
-	// Migrate database
+	// Migrate to database
 	if err := database.Migrate(db); err != nil {
 		log.Fatalf("error migrating database: %v", err)
 	}
 
 	// Start server
 	app := fiber.New()
+
+	app.Use(func(c *fiber.Ctx) error {
+		c.Locals("redis", rdb)
+		return c.Next()
+	})
 
 	app.Use(logger.New(logger.Config{
 		Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
