6662b600e8908057dc40970f5ae2bdaf92321096
Ayush <spanhornet@pm.me>
2025-06-16 22:39:02 -0500

    feat: create Go workspace to enhance Go REST API with database package
    - Created go.work and go.work.sum for managing Go modules.
    - Updated main.go to load environment variables and connect to the PostgreSQL database.
    - Implemented database migration functionality in the Go REST API.

--- apps/go-rest-api/main.go
+++ apps/go-rest-api/main.go
@@ -1,13 +1,40 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+
+	"github.com/joho/godotenv"
+
+	"github.com/spanhornet/brambles/packages/database"
 )
 
 func main() {
+	// Load environment variables
+	if err := godotenv.Load(); err != nil {
+		log.Println("no .env file found, relying on environment")
+	}
+
+	// Load environment variables
+	dsn := os.Getenv("SUPABASE_DATABASE_CONNECTION_STRING")
+	if dsn == "" {
+		log.Fatal("`SUPABASE_DATABASE_CONNECTION_STRING` is not set")
+	}
+
+	// Connect to database
+	db, err := database.ConnectToDatabase(dsn)
+	if err != nil {
+		log.Fatalf("error connecting to database: %v", err)
+	}
+
+	// Migrate to database
+	if err := database.Migrate(db); err != nil {
+		log.Fatalf("error migrating database: %v", err)
+	}
+
 	// Initialize app
 	app := fiber.New()
 
